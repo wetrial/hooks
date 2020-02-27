0a094d1832edea477350907f20e85249837e7925
xiexingen <1002275364@qq.com>
2020-02-27 22:22:22 +0800

    支持qiankun

--- .umirc.js
+++ .umirc.js
@@ -26,10 +26,7 @@ export default {
     logo: 'https://avatars2.githubusercontent.com/u/40448889?s=66&v=4',
     title: 'Wetrial Hooks',
     include: ['packages/hooks/src', 'packages/use-request'],
-    locales: [
-      ['zh-CN', '中文'],
-      ['en-US', 'English'],
-    ],
+    locales: [['zh-CN', '中文']],
   },
   plugins: [
     [
@@ -39,5 +36,6 @@ export default {
         headScripts: ['https://hm.baidu.com/hm.js?a3636d814818bccb02a7991d78ba3048'],
       },
     ],
+    ['@umijs/plugin-qiankun'],
   ],
 };
