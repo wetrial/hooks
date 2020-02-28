2c55df47e30bf61234b680e84eae18ee9dc833df
xiexingen <1002275364@qq.com>
2020-02-28 10:14:05 +0800

    部署支持browser history & hash

--- .umirc.js
+++ .umirc.js
@@ -34,6 +34,15 @@ export default {
       {
         pwa: true,
         headScripts: ['https://hm.baidu.com/hm.js?a3636d814818bccb02a7991d78ba3048'],
+        scripts: [
+          // 由于github不支持url重写，history-route模式下会跳转到404 404页面会对路由进行处理将路由转换成?path=xxx/xxx这种形式，首页需要对这种进行处理通过window.g_history.push()进行跳转
+          `(function(g_history){
+            if(g_history&&g_history.location&&g_history.location.query&&g_history.location.query.path){
+              var hash=g_history.location.hash
+              g_history.push({pathname:g_history.location.query.path,hash:hash})
+            }
+          }(window.g_history))`,
+        ],
       },
     ],
     ['@umijs/plugin-qiankun'],
