7a100dad2ed0ea3ac2c7de6f98cb4f036f909548
xiexingen <1002275364@qq.com>
2020-04-16 14:31:52 +0800

    不处理自动跳转问题

--- .umirc.ts
+++ .umirc.ts
@@ -35,14 +35,14 @@ export default defineConfig({
       path: 'https://hooks.umijs.org/zh-CN/hooks',
     },
   ],
-  scripts: [
-    // 由于github不支持url重写，history-route模式下会跳转到404 404页面会对路由进行处理将路由转换成?path=xxx/xxx这种形式，首页需要对这种进行处理通过window.g_history.push()进行跳转
-    `(function(g_history){
-      if(g_history&&g_history.location&&g_history.location.query&&g_history.location.query.path){
-        var hash=g_history.location.hash
-        g_history.push({pathname:g_history.location.query.path,hash:hash})
-      }
-    }(window.g_history))`,
-  ],
+  // scripts: [
+  //   // 由于github不支持url重写，history-route模式下会跳转到404 404页面会对路由进行处理将路由转换成?path=xxx/xxx这种形式，首页需要对这种进行处理通过window.g_history.push()进行跳转
+  //   `(function(g_history){
+  //     if(g_history&&g_history.location&&g_history.location.query&&g_history.location.query.path){
+  //       var hash=g_history.location.hash
+  //       g_history.push({pathname:g_history.location.query.path,hash:hash})
+  //     }
+  //   }(window.g_history))`,
+  // ],
   //plugins: ['@umijs/plugin-qiankun'],
 });
